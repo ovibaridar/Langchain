38990be6179d6f0fb6399393a04e40221afef644
ovibaridar <ovibaridar06@gmail.com>
2025-02-17 04:47:41 +0600

    day1 , 17/02/2025

--- TestDay1.py
+++ TestDay1.py
@@ -3,5 +3,5 @@ from ApiKey import ApiKey
 os.environ["GROQ_API_KEY"] = ApiKey
 from langchain.chat_models import init_chat_model
 model = init_chat_model("llama3-8b-8192", model_provider="groq")
-output = model.invoke("give me a  model name which can find for me deep information using number or gmail or photos")
+output = model.invoke("")
 print(output.content)
